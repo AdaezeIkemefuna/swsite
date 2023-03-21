640ae87645bb3545089843aaee6c187701ca2bf0
Ada-ikemefuna <avhyrus@gmail.com>
2023-03-21 21:18:03 +0100

    css contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,139 @@
+"use client";
 import styles from "./page.module.scss";
 import { MdLocationOn } from "react-icons/md";
 import { HiPhone } from "react-icons/hi";
+import { useState } from "react";
+import { InfinitySpin } from "react-loader-spinner";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Image from "next/image";
+interface Tutorial {
+  from?: string;
+  message: string;
+  subject: string;
+}
 export default function page() {
+  const [name, setName] = useState("");
+  const [from, setFrom] = useState("");
+  const [subject, setSubject] = useState("");
+
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const contact = async ({
+    from,
+
+    subject,
+
+    message,
+  }: Tutorial) => {
+    try {
+      const response = await fetch("https://m-kozm.onrender.com/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          from,
+
+          subject,
+
+          message,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        console.log("ok");
+        setLoading(false);
+        toast.success("message sent", {
+          position: toast.POSITION.BOTTOM_LEFT,
+        });
+      } else {
+      }
+    } catch (err) {
+      console.log(err, "not working");
+      toast.error("failed to send message", {
+        position: toast.POSITION.BOTTOM_LEFT,
+      });
+
+      setLoading(false);
+    } finally {
+      setSubject("");
+      setMessage("");
+      setFrom("");
+      setPhone("");
+      setLoading(false);
+    }
+  };
+
+  const contactHandler = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    contact({ from, subject, message });
+  };
   return (
     <div className={styles.mainContact}>
+      <ToastContainer />
       <section className={styles.contactWrapper}>
-        <aside>
-          <h1>Use the form below to contact us</h1>
-          <input type="text" placeholder="Full Name" className={styles.input} />
-          <input
-            type="number"
-            placeholder="Phone Number"
-            className={styles.input}
-          />
-          <input
-            type="text"
-            placeholder="Email Address"
-            className={styles.input}
-          />
-          <textarea
-            style={{ width: "100%" }}
-            placeholder="Message"
-            className={styles.input}
-          ></textarea>
-          <button className={styles.btn2}>send message</button>
-        </aside>
+        <form onSubmit={(e) => contactHandler(e)}>
+          <aside>
+            <h1>Use the form below to contact us</h1>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className={styles.input}
+              required
+              value={from}
+              onChange={(e) => setFrom(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              className={styles.input}
+              required
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email Address"
+              className={styles.input}
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <textarea
+              style={{ width: "100%" }}
+              placeholder="Message"
+              className={styles.input}
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            {loading === true ? (
+              <div
+                style={{
+                  marginTop: "2rem",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "0rem",
+                  backgroundColor: "#e7992a",
+                  borderRadius: "6px",
+                  textAlign: "center",
+                  minWidth: "100px",
+                }}
+              >
+                <InfinitySpin width="100" color="#fff" />
+              </div>
+            ) : (
+              <button className={styles.btn2}>send message</button>
+            )}{" "}
+          </aside>
+        </form>
         <aside>
           <h1>Get in touch</h1>
           <p>
--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,36 +1,178 @@
+"use client";
+import { useState } from "react";
+import { InfinitySpin } from "react-loader-spinner";
 import styles from "./page.module.scss";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+interface Tutorial {
+  date: string;
+  name: string;
+  time: string;
+  email?: string;
+  phone: string;
+  NOG: string;
+  message: string;
+}
+
 export default function page() {
+  const [date, setDate] = useState("");
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [NOG, setNOG] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const reserve = async ({
+    date,
+    name,
+    time,
+    email,
+    phone,
+    NOG,
+    message,
+  }: Tutorial) => {
+    try {
+      const response = await fetch("https://m-kozm.onrender.com/reservation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          date,
+          name,
+          time,
+          email,
+          phone,
+          NOG,
+          message,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok || response.status === 204) {
+        console.log("ok");
+        toast.success("reservation placed", {
+          position: toast.POSITION.BOTTOM_LEFT,
+        });
+        setLoading(false);
+      } else {
+      }
+    } catch (err) {
+      console.log(err, "not working");
+      toast("failed to place reservation");
+    } finally {
+      setDate("");
+      setEmail("");
+      setMessage("");
+      setNOG("");
+      setName("");
+      setPhone("");
+      setNOG("");
+    }
+    setLoading(false);
+  };
+
+  const sendReservation = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    reserve({ date, name, time, email, phone, NOG, message });
+  };
   return (
-    <div className={styles.mainReservation}>
-      <h1 className={styles.after}>Reservations</h1>
-      <aside className={styles.inputSection}>
-        <div>
-          <input type="date" placeholder="Date" className={styles.input} />
-          <input type="text" placeholder="Name" className={styles.input} />
-        </div>
-        <div>
-          <input type="time" placeholder="Time" className={styles.input} />
-          <input
-            type="email"
-            placeholder="Email Address"
-            className={styles.input}
-          />
-        </div>
-        <div>
-          <input
-            type="number"
-            placeholder="Number of Guests"
-            className={styles.input}
-          />
-          <input type="number" placeholder="Phone" className={styles.input} />
-        </div>
-      </aside>
-      <textarea
-        placeholder="Message"
-        className={styles.input}
-        style={{ minWidth: "78%", minHeight: "180px", maxWidth: "100%" }}
-      ></textarea>
-      <button className={styles.btn2}>make a reservation</button>
-    </div>
+    <>
+      <ToastContainer />
+
+      <form
+        className={styles.mainReservation}
+        onSubmit={(e) => sendReservation(e)}
+      >
+        <h1 className={styles.after}>Reservations</h1>
+        <aside className={styles.inputSection}>
+          <div>
+            <input
+              type="text"
+              placeholder="Date"
+              className={styles.input}
+              required
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              className={styles.input}
+              value={name}
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Time"
+              className={styles.input}
+              required
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className={styles.input}
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Number of Guests"
+              className={styles.input}
+              required
+              value={NOG}
+              onChange={(e) => setNOG(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Phone"
+              className={styles.input}
+              required
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+        </aside>
+        <textarea
+          placeholder="Message"
+          className={styles.input}
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          style={{ minWidth: "78%", minHeight: "180px", maxWidth: "100%" }}
+        ></textarea>
+
+        {loading === true ? (
+          <div
+            style={{
+              marginTop: "2rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "0rem",
+              backgroundColor: "#e7992a",
+              borderRadius: "6px",
+              textAlign: "center",
+              minWidth: "220px",
+            }}
+          >
+            <InfinitySpin width="100" color="#fff" />
+          </div>
+        ) : (
+          <button className={styles.btn2}>make a reservation</button>
+        )}
+      </form>
+    </>
   );
 }
